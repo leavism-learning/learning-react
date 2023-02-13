ac68b5b4c16e526bba97abd8d18f85a098b28ffa
Huy Dang <ghuydang@gmail.com>
2023-02-13 09:10:49 -0700

    Errorboundary and fallback UI

--- src/App.js
+++ src/App.js
@@ -101,6 +101,31 @@ function CountButton() {
   );
 }
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change to true to enable error
+    this.state = { hasError: true };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(err, errInfo) {
+    console.log('something went horribly wrong', err, errInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className='error-boundary'>
+          <h3>Fallback UI</h3>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 function App() {
   return (
     <>
@@ -117,6 +142,12 @@ function App() {
         <Count />
         <CountButton />
       </CountProvider>
+      <div>
+        <h3>Outside the error boundary</h3>
+        <ErrorBoundary>
+          <h3>Inside the error boundary</h3>
+        </ErrorBoundary>
+      </div>
     </>
   );
 }

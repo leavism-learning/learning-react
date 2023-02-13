fd253eb6aaeb50f6516ca217895523ab249e22d7
Huy Dang <ghuydang@gmail.com>
2023-02-13 10:39:34 -0700

    Abstracted ErrorBoundary to own class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
+import ErrorBoundary from './ErrorBoundary';
 import './App.css';
 
 function Card(props) {
@@ -99,31 +100,6 @@ function CountButton() {
       Increment
     </button>
   );
-}
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    // Change to true to enable error
-    this.state = { hasError: true };
-  }
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-  componentDidCatch(err, errInfo) {
-    console.log('something went horribly wrong', err, errInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className='error-boundary'>
-          <h3>Fallback UI</h3>
-        </div>
-      );
-    }
-    return this.props.children;
-  }
 }
 
 function App() {
